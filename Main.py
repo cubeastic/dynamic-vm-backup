3c76676d9535b2640836bb3f91264c620ef1ac14
Guzzur <frazykov@gmail.com>
2017-12-08 23:36:43 +0200

    fix

--- Main.py
+++ Main.py
@@ -15,29 +15,34 @@ if __name__ == "__main__":
 
     backup_path = ""
     vbox_clone_path = ""
+    remote_machine = ["", "", "", ""]  # ip, user, pass, path
 
     # Use all the configuration data
     for hold in config.holder:
         id = ""
         name = ""
+        try:
+            if hold[1] == "vm":
+                try:
+                    id = hold[2]["id"]
+                except:
+                    pass
+                try:
+                    name = hold[2]["name"]
+                except:
+                    print "Unnamed machine found in config. Exiting task"
+                    exit(1)
 
-        if hold[1] == "vm":
-            try:
-                id = hold[2]["id"]
-            except:
-                pass
-            try:
-                name = hold[2]["name"]
-            except:
-                print "Unnamed machine found in config. Exiting task"
+                vm_list.append([id, name])
 
-            vm_list.append([id, name])
+            if hold[1] == "vbox":
+                vbox_clone_path = hold[2]["clone_path"]
 
-        if hold[1] == "vbox":
-            vbox_clone_path = hold[2]["clone_path"]
-
-        if hold[1] == "backup":
-            backup_path = hold[2]["path"]
+            if hold[1] == "backup":
+                backup_path = hold[2]["path"]
+        except:
+            print "The config file is not arranged properly"
+            exit(1)
 
     vms = VmManager(vm_list)
     vms.stop_all_machines()
